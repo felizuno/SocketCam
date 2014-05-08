2bdf0b645beb719d1a4ff3686fdbb6b0a10a8db2
Kevin Ewing <notmyuwmail@gmail.com>
2014-05-07 20:19:42 -0700

    cleanup, remove

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 var ws = require("ws");
 var port = 9999;
-var socketServer = new ws.Server({port: port}); 
+var socketServer = new ws.Server({port: port});
 
 socketServer.on("connection", function(socket) {
   cameraServer.addClient(socket);
 });
 
 var cameraServer = {
-  addClient: function(socket) {
+  _clients: [],
 
+  addClient: function(socket) {
+    _clients.push(new Client(socket));
+    console.log('New client, total is ' + this._clients.length);
+  },
+
+  removeUser: function(client) {
+    this._clients.splice(this._clients.indexOf(client), 1, 0);
+    console.log('Lost client, total is ' + this._clients.length);
   }
 };
+
+
+var Client = function(socket) {
+  this.socket = socket;
+  this.socket.on('close', function() { cameraServer.removeUser(this); });
+};
+
+console.log('Socket server listening on port ' + port );
