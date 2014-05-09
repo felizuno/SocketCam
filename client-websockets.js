f880d89bc0f358ad32f331697c2ac4f629fb27eb
Kevin Ewing <notmyuwmail@gmail.com>
2014-05-08 20:22:51 -0700

    fixing up and vanity

--- client-websockets.js
+++ client-websockets.js
@@ -1,41 +1,49 @@
-/// More or less unchanged, credit to Ian Gilman
+/// Basic structure from Ian Gilman
 /// Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+var socketUrl = "ws://< RASPBERRY PI IP ADDRESS >:9999";
 
-window.SocketServer = function(callback) {
-  this.callback = callback;
+window.SocketServer = function(cfg) {
+  var noOp = function() { return false; },
+      defaults = {
+        connectCallback: noOp,
+        disconnectCallback: noOp,
+        drawHandler: noOp
+      };
+
+  this.callbacks = (cfg.connectCallback 
+    && cfg.disconnectCallback 
+    && cfg.drawHandler)
+    ? cfg
+    : defaults;
+
   this.connection = null;
 };
 
 SocketServer.prototype = {
   send: function(method, data, next) {
-    var self = this;
     if (method == "connect") {
       var SupportedSocket = window.WebSocket || window.MozWebSocket;
       if (SupportedSocket) {
-        var url = "ws://< RASPBERRY PI IP ADDRESS >:9999";
-        this.connection = new SupportedSocket(url);
+        this.connection = new SupportedSocket(socketUrl);
         this.bindMessageHandlers();
       }
       
       data.code = (SupportedSocket) ? "success" : "failure";
       next(data);
     } else if (method == "message") {
-      if (this.connection.readyState != 1) return;
+      if (this.connection && this.connection.readyState != 1) return;
       this.connection.send(JSON.stringify({ method: method, data: data }));
     }
   },
 
   bindMessageHandlers: function(data, next) {
-    this.connection.onopen = function() { 
-      // self.connection.send(JSON.stringify({
-      //   method: "memberEnter", 
-      //   data: data
-      // }));
+    this.connection.onopen = function() {
+      this.connectCallback(this);
     };
     
     this.connection.onmessage = function(event) { 
       var envelope = JSON.parse(event.data);
-      self.callback(envelope.method, envelope.data);
+      this.drawHandler(envelope.method, envelope.data);
     };
     
     this.connection.onclose = function(event) { 
